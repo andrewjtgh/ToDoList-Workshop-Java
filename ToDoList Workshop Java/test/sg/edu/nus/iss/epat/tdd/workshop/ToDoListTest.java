2e9c3ac56d0cb3096519a6a019aa2024fdcfd09f
andrewtan2000 <andrew_tan2000@hotmail.com>
2023-01-18 22:36:57 +0800

    Passed testRemoveTask

--- ToDoList Workshop Java/test/sg/edu/nus/iss/epat/tdd/workshop/ToDoListTest.java
+++ ToDoList Workshop Java/test/sg/edu/nus/iss/epat/tdd/workshop/ToDoListTest.java
@@ -7,52 +7,58 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 public class ToDoListTest extends TestCase {
-   // Define Test Fixtures
+	// Define Test Fixtures
 	private ToDoList todolist;
 	private Task task;
-	
-   public ToDoListTest() {
-      super();
-   }
+	private Task task2;
 
-   @Before
-   public void setUp() throws Exception {
-      // Initialise Test Fixtures
-	   todolist = new ToDoList();
-	   task = new Task("Desc 1");
+	public ToDoListTest() {
+		super();
+	}
 
-   }
+	@Before
+	public void setUp() throws Exception {
+		// Initialise Test Fixtures
+		todolist = new ToDoList();
+		task = new Task("Desc 1");
+		task2 = new Task("Desc 2");
+	}
 
-   @After
-   public void tearDown() throws Exception {
-      // Uninitialised test Fixtures
-   }
+	@After
+	public void tearDown() throws Exception {
+		// Uninitialised test Fixtures
+	}
 
-   @Test
-   public void testAddTask() {
-	   todolist.addTask(task);
-	      assertTrue(todolist.getAllTasks().contains(task));
-   }
+	@Test
+	public void testAddTask() {
+		todolist.addTask(task);
+		assertTrue(todolist.getAllTasks().contains(task));
+	}
 
-   @Test
-   public void testIsEmpty() {
-	   todolist.addTask(task);
-	   todolist.removeTask(task.getDescription());
-	      assertTrue(todolist.getAllTasks().isEmpty());
-   }   
-   
-   @Test
-   public void testGetStatus() {
-      fail("Not implemented yet");
-   }
+	@Test
+	public void testIsEmpty() {
+		todolist.addTask(task);
+		todolist.removeTask(task.getDescription());
+		assertTrue(todolist.getAllTasks().isEmpty());
+	}
 
-   @Test
-   public void testRemoveTask() {
-      fail("Not implemented yet");
-   }
+	@Test
+	public void testGetStatus() {
+		fail("Not implemented yet");
+	}
 
-   @Test
-   public void testGetCompletedTasks() {
-      fail("Not implemented yet");
-   }
+	@Test
+	public void testRemoveTask() {
+		// fail("Not implemented yet");
+		todolist.addTask(task);
+		todolist.addTask(task2);
+		todolist.removeTask(task.getDescription());
+		assertFalse(todolist.getAllTasks().contains(task));
+
+	}
+
+	@Test
+	public void testGetCompletedTasks() {
+		fail("Not implemented yet");
+	}
 }
