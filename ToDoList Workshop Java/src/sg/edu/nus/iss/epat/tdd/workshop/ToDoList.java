c4ca59e67f699f0b4cd47d43d605639eb12b9a2a
andrewtan2000 <andrew_tan2000@hotmail.com>
2023-01-19 14:58:31 +0800

    Passed getCompletedTasks

--- ToDoList Workshop Java/src/sg/edu/nus/iss/epat/tdd/workshop/ToDoList.java
+++ ToDoList Workshop Java/src/sg/edu/nus/iss/epat/tdd/workshop/ToDoList.java
@@ -1,40 +1,55 @@
 package sg.edu.nus.iss.epat.tdd.workshop;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
 public class ToDoList {
-   private HashMap<String, Task> tasks = new HashMap<String, Task>();
+	private HashMap<String, Task> tasks = new HashMap<String, Task>();
 
-   public void addTask(Task task) {
-      // Add code here
-	   tasks.put(task.getDescription(), task);
-   }
+	public void addTask(Task task) {
+		// Add code here
+		tasks.put(task.getDescription(), task);
+	}
 
-   public void completeTask(String description) {
-      // Add code here
-   }
+	public void completeTask(String description) {
+		// Add code here
+		Task task = null;
+		if ((task = tasks.get(description)) != null)
+			;
+		task.setComplete(true);
+	}
 
-   public boolean getStatus(String description) {
-      // Add code here
-      return false;
-   }
+	public boolean getStatus(String description) {
+		// Add code here
+		Task task = null;
+		if ((task = tasks.get(description)) != null)
+			;
+		return task.isComplete();
+	}
 
-   public Task getTask(String description) {
-      // Add code here
-      return null;
-   }
+	public Task getTask(String description) {
+		// Add code here
+		return null;
+	}
 
-   public Task removeTask(String description) {
-      // Add code here
-      return null;
-   }
+	public Task removeTask(String description) {
+		// Add code here
+		return tasks.remove(description);
+	}
 
-   public Collection<Task> getAllTasks() {
-      return tasks.values();
-   }
+	public Collection<Task> getAllTasks() {
+		return tasks.values();
+	}
 
-   public Collection<Task> getCompletedTasks() {
-      // Add code here
-      return null;
-   }
+	public Collection<Task> getCompletedTasks() {
+		Collection<Task> completedTasks = new ArrayList<Task>();
+
+		for (Task task : getAllTasks()) {
+			if (task.isComplete())
+				completedTasks.add(task);
+		}
+
+		return completedTasks;
+	}
 }
--- ToDoList Workshop Java/test/sg/edu/nus/iss/epat/tdd/workshop/ToDoListTest.java
+++ ToDoList Workshop Java/test/sg/edu/nus/iss/epat/tdd/workshop/ToDoListTest.java
@@ -1,6 +1,11 @@
 package sg.edu.nus.iss.epat.tdd.workshop;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.junit.After;
 import org.junit.Before;
@@ -13,6 +18,7 @@ public class ToDoListTest extends TestCase {
 	private ToDoList todolist;
 	private Task task;
 	private Task task2;
+	private Task task3;
 
 	public ToDoListTest() {
 		super();
@@ -24,6 +30,7 @@ public class ToDoListTest extends TestCase {
 		todolist = new ToDoList();
 		task = new Task("Desc 1");
 		task2 = new Task("Desc 2");
+		task3 = new Task("Desc 3");
 	}
 
 	@After
@@ -65,6 +72,19 @@ public class ToDoListTest extends TestCase {
 
 	@Test
 	public void testGetCompletedTasks() {
-		fail("Not implemented yet");
+		// fail("Not implemented yet");
+		todolist.addTask(task);
+		todolist.addTask(task2);
+		todolist.addTask(task3);
+
+		todolist.completeTask("Desc 1");
+		todolist.completeTask("Desc 3");
+
+		Collection<Task> tasks = todolist.getCompletedTasks();
+		assertEquals(tasks.size(), 2);
+		assertTrue(tasks.contains(task));
+		assertFalse(tasks.contains(task2));
+		assertTrue(tasks.contains(task3));
+
 	}
 }
